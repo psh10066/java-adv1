728d36840962f303250c8103f1951b93966ea6d2
이호준 <psh10066@gmail.com>
2024-10-24 21:04:47 +0900

    [스레드 생성과 실행] 스레드 시작2

---
+++ src/thread/start/BadThreadMain.java
@@ -0,0 +1,16 @@
+package thread.start;
+
+public class BadThreadMain {
+
+    public static void main(String[] args) {
+        // Java 기본 스레드 : main
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        HelloThread helloThread = new HelloThread();
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+        helloThread.run(); // run 직접 실행
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+}
