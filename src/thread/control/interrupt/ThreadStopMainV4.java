100f7db24448fc892decc4f7df55a3f0c6dd41e9
이호준 <psh10066@gmail.com>
2024-10-29 21:38:07 +0900

    [스레드 제어와 생명 주기2] 인터럽트 - 시작4

---
+++ src/thread/control/interrupt/ThreadStopMainV4.java
@@ -0,0 +1,39 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV4 {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(100);
+        log("작업 중단 지시 thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted()); // true
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) { // interrupted()는 인터럽트 상태가 true인 경우 true를 반환한 이후 다시 false로 바꾼다.
+                log("작업중");
+            }
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted()); // false
+
+            try {
+                log("자원 정리");
+                Thread.sleep(1000);
+                log("자원 종료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted()); // false
+            }
+            log("작업 종료");
+        }
+    }
+}
